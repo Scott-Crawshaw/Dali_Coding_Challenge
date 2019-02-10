fc66a762a344edc002cf901c2c7687c1d8fe5b0b
Scott Crawshaw <scott.crawshaw@gmail.com>
2019-02-10 18:51:28 -0500

    Created the clickable functionality on the terms page

--- app/src/main/java/com/example/dali_coding_challenge/Annual.java
+++ app/src/main/java/com/example/dali_coding_challenge/Annual.java
@@ -1,7 +1,9 @@
 package com.example.dali_coding_challenge;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 
 public class Annual extends AppCompatActivity {
@@ -12,5 +14,41 @@ public class Annual extends AppCompatActivity {
         setContentView(R.layout.activity_annual);
         getSupportActionBar().setTitle("Terms");
 
+    }
+
+    public void fall(View view) {
+        Bundle bundle = new Bundle();
+        //show names for all on terms
+        bundle.putChar("term", 'F');
+        Intent intent = new Intent(this, People.class);
+        intent.putExtras(bundle);
+        startActivity(intent);
+    }
+
+    public void winter(View view) {
+        Bundle bundle = new Bundle();
+        //show names for all on terms
+        bundle.putChar("term", 'W');
+        Intent intent = new Intent(this, People.class);
+        intent.putExtras(bundle);
+        startActivity(intent);
+    }
+
+    public void spring(View view) {
+        Bundle bundle = new Bundle();
+        //show names for all on terms
+        bundle.putChar("term", 'S');
+        Intent intent = new Intent(this, People.class);
+        intent.putExtras(bundle);
+        startActivity(intent);
+    }
+
+    public void summer(View view) {
+        Bundle bundle = new Bundle();
+        //show names for all on terms
+        bundle.putChar("term", 'X');
+        Intent intent = new Intent(this, People.class);
+        intent.putExtras(bundle);
+        startActivity(intent);
     }
 }
